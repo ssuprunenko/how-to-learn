94c0b42ba941a47ad55ae8dd52821afb32762dbf
Sergey Suprunenko <suprunenko.s@gmail.com>
2020-11-21 16:01:10 +0100

    Add user likes page to the admin panel

--- lib/app_web/admin/category_admin.ex
+++ lib/app_web/admin/category_admin.ex
@@ -3,6 +3,16 @@ defmodule App.Admin.CategoryAdmin do
     "Categories"
   end
 
+  def index(_) do
+    [
+      id: nil,
+      name: nil,
+      slug: nil,
+      is_published: nil,
+      inserted_at: nil
+    ]
+  end
+
   def form_fields(_) do
     [
       name: nil,
